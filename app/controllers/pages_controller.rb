d7c0cec3a9a673b8d6b82f9958289c2c647caad2
Quentin GOURDON <quentin.gourdon@gmail.com>
2019-01-16 03:09:19 +0100

    background

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,14 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def print_random_char(string)
+    array = []
+    string.each_char do |c|
+      array << c
+    end
+    new_array = array.shuffle
+    new_array[1...2].join
+  end
+
 end
