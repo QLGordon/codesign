1a0103b052b881e32b5c417f598f3daa32670832
Quentin GOURDON <quentin.gourdon@gmail.com>
2019-01-21 10:37:28 +0100

    up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,36 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    font_size(1, 2, 4, 288)
+    translate
+    rotate
   end
 
-  def random_char(string)
-    array = []
-    string.each_char do |c|
-      array << c
+  private
+
+  def font_size(a,b,c,d)
+    @font_size = []
+    d.times do
+      @font_size << 48
     end
-    new_array = array.shuffle
-    new_array[1...2].join
+    c.times do
+      @font_size << 49
+    end
+    b.times do
+      @font_size << 50
+    end
+    a.times do
+      @font_size << 51
+    end
+  end
+  def translate
+    @translate = []
+    @translate << 1
+    @translate << 2
+    @translate << 3
+  end
+  def rotate
+    @rotate = (-11..11).to_a
   end
 
 end
