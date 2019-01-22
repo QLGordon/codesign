6b90a00badbd7d3b8f6e0bb4c6e5c5bfb03d027f
Quentin GOURDON <quentin.gourdon@gmail.com>
2019-01-22 11:41:30 +0100

    Update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,28 +12,33 @@ class PagesController < ApplicationController
   def font_size(a,b,c,d)
     @font_size = []
     d.times do
-      @font_size << 48
+      @font_size << 24
     end
     c.times do
-      @font_size << 49
+      @font_size << 48
     end
     b.times do
-      @font_size << 50
+      @font_size << 72
     end
     a.times do
-      @font_size << 51
+      @font_size << 144
     end
   end
 
   def translate
-    @translate = []
-    @translate << 1
-    @translate << 2
-    @translate << 3
+    # @translate = []
+    # @translate << 1
+    # @translate << 2
+    # @translate << 3
+    @translate = (-500..500).to_a
   end
 
   def rotate
-    @rotate = (-11..11).to_a
+    # @rotate = []
+    # @rotate << 60
+    # @rotate << 120
+    # @rotate << 180
+    @rotate = (-360..360).to_a
   end
 
 end
