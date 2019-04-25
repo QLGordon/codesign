a5763a0c9b85636f2453ad8dff807cc0d54c56fe
Quentin GOURDON <quentin.gourdon@gmail.com>
2019-04-25 11:24:10 +0200

    Up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
 import "bootstrap";
+
+const headerScroll = () => {
+  const header = document.querySelector('header');
+  document.addEventListener("scroll", function(){
+    if (document.scrollTop > 50 || document.scrollTop < 350) {
+    header.className = "slideUp";
+  }
+  });
+};
+
+headerScroll();
+console.log("helloguuurl");
