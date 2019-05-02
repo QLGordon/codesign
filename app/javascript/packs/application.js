e1b45b8dc5e4e8c2299fd9eaae91a4861c262e30
Quentin GOURDON <quentin.gourdon@gmail.com>
2019-05-02 14:45:33 +0200

    Up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,16 +1,16 @@
 import "bootstrap";
 
-const headerScroll = () => {
-  let prevScrollpos = window.pageYOffset;
-  window.onscroll = function() {
-    let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos || document.querySelector('.content').onmouseover) {
-      document.querySelector('.content').style.opacity = "1";
-    } else {
-      document.querySelector('.content').style.opacity = ".1";
-    }
-    prevScrollpos = currentScrollPos;
-  }
-};
+// const headerScroll = () => {
+//   let prevScrollpos = window.pageYOffset;
+//   window.onscroll = function() {
+//     let currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos || document.querySelector('.head-op').onmouseover) {
+//       document.querySelector('.head-op').style.opacity = "1";
+//     } else {
+//       document.querySelector('.head-op').style.opacity = ".1";
+//     }
+//     prevScrollpos = currentScrollPos;
+//   }
+// };
 
-headerScroll();
+// headerScroll();
